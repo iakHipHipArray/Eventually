9b69efe72b037cd4491670929043a33b868197c2
Robin Pillar <rpillar72@gmail.com>
2020-01-14 09:50:25 +0000

    created basic login widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:EVENTually/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'EVENTually'),
+      home: LoginPage(),
     );
   }
 }
