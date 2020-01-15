65443b546bbd74d4001e5c4259ab324a9afed18a
iakHipHipArray <inshirah.akhan@gmail.com>
2020-01-15 10:30:23 +0000

    Updated location.dart to now display the user's current location on the app itself instead of just in the console

--- lib/Pages/location.dart
+++ lib/Pages/location.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+class LocationGetter extends StatefulWidget {
+  @override
+  _LocationState createState() => _LocationState();
+}
 
-class LocationGetter extends StatelessWidget {
+class _LocationState extends State<LocationGetter> {
+  String _locationMessage = "";
+
   void _displayCurrentLocation() async {
     final location = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-   print('Current location lat long ' + location.latitude.toString() + " - " + location.longitude.toString());
+    print('Current location lat long ' +
+        location.latitude.toString() +
+        " - " +
+        location.longitude.toString());
+
+    setState(() {
+      _locationMessage = "${location.latitude}, ${location.longitude}";
+    });
   }
 
   @override
@@ -18,7 +31,7 @@ class LocationGetter extends StatelessWidget {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(""),
+                Text(_locationMessage),
                 FlatButton(
                   child: Text("Choose your current location",
                       style: TextStyle(color: Colors.white)),
