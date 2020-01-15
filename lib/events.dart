b931b7ddcb6dd88f4c8cd1a5efdd0cc5ef46a49e
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2020-01-15 09:52:38 +0000

    added querying to show only events specific to logged in user

--- lib/events.dart
+++ lib/events.dart
@@ -12,29 +12,29 @@ class _EventsPageState extends State<EventsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Events'),
-      ),
-      body: _buildBody(context)
-    );
+        appBar: AppBar(
+          title: Text('Your Events'),
+        ),
+        body: _buildBody(context));
   }
 }
 
 Widget _buildBody(BuildContext context) {
   return StreamBuilder<QuerySnapshot>(
-    stream: Firestore.instance.collection('events').snapshots(),
-    builder: (context, snapshot) {
-      if(!snapshot.hasData) return Text('...Loading');
-      return _buildList(context, snapshot.data.documents);
-    }
-  );
+      stream: Firestore.instance
+          .collection('events')
+          .where('attendees', arrayContains: 'ryan1214')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Text('...Loading');
+        return _buildList(context, snapshot.data.documents);
+      });
 }
 
 Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
   return ListView(
-    padding: const EdgeInsets.only(top: 20.0),
-    children: snapshot.map((data) => _buildListItem(context,data)).toList()
-  );
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList());
 }
 
 Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
@@ -42,43 +42,48 @@ Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
 
   return Padding(
     key: ValueKey(record.eventName),
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), 
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
     child: Container(
-      child: Column(
-        children: <Widget>[
-          Text(record.eventName),
-          SizedBox(height: 10),
-          Text(record.summary),
-          SizedBox(height: 10),
-          Row(children: <Widget>[
-            record.finalDate != null ? Text('Date: $record.finalDate') :  Text('Date: TBC'),
+        child: Column(
+      children: <Widget>[
+        Text(record.eventName),
+        SizedBox(height: 10),
+        Text(record.summary),
+        SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            record.finalDate != null
+                ? Text('Date: $record.finalDate')
+                : Text('Date: TBC'),
             Spacer(),
-            record.finalLocation != null ? Text('Location: $record.finalDate') :  Text('Location: TBC')
-          ],)
-       ],
-      )
-    ),
+            record.finalLocation != null
+                ? Text('Location: $record.finalDate')
+                : Text('Location: TBC')
+          ],
+        )
+      ],
+    )),
   );
 }
 
 class Record {
- final String eventName;
- final String summary;
- final finalDate;
- final finalLocation;
- final DocumentReference reference;
+  final String eventName;
+  final String summary;
+  final finalDate;
+  final finalLocation;
+  final DocumentReference reference;
 
- Record.fromMap(Map<String, dynamic> map, {this.reference})
-     : assert(map['eventName'] != null),
-       assert(map['summary'] != null),
-       eventName = map['eventName'],
-       summary = map['summary'],
-       finalDate = map['finalDate'],
-       finalLocation = map['finalLocation'];
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['eventName'] != null),
+        assert(map['summary'] != null),
+        eventName = map['eventName'],
+        summary = map['summary'],
+        finalDate = map['finalDate'],
+        finalLocation = map['finalLocation'];
 
- Record.fromSnapshot(DocumentSnapshot snapshot)
-     : this.fromMap(snapshot.data, reference: snapshot.reference);
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
- @override
- String toString() => "Record<$eventName:$summary>";
+  @override
+  String toString() => "Record<$eventName:$summary>";
 }
