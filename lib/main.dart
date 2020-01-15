afae5ca85346a75f155161c21b4816b344d18023
iakHipHipArray <inshirah.akhan@gmail.com>
2020-01-15 15:46:54 +0000

    Updated main.dart styling to have a consistent theme with the map markers

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EVENTually',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
       home: Map(),
     );
