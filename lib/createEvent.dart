cce933d949100e02e089cc51d6555b0ab7b70989
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2020-01-16 16:05:00 +0000

    started createEvent form

---
+++ lib/createEvent.dart
@@ -0,0 +1,103 @@
+import 'package:EVENTually/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_multiselect/flutter_multiselect.dart';
+
+class CreateEvent extends StatefulWidget {
+  @override
+  _CreateEventState createState() => _CreateEventState();
+}
+
+class _CreateEventState extends State<CreateEvent> {
+  final _titleController = TextEditingController();
+  final _summaryController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            Column(
+              children: <Widget>[
+                SizedBox(height: 16.0),
+                Text('EVENTually'),
+              ],
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(filled: true, labelText: 'Title'),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            TextField(
+              controller: _summaryController,
+              decoration:
+                  InputDecoration(filled: true, labelText: 'Description'),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            MultiSelect(
+                autovalidate: false,
+                titleText: 'Add Friends',
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select one or more friends(s)';
+                  }
+                },
+                errorText: 'Please select one or more friends(s)',
+                dataSource: [
+                  {
+                    "display": "Ryan",
+                    "value": 1,
+                  },
+                  {
+                    "display": "Robin",
+                    "value": 2,
+                  },
+                  {
+                    "display": "Narae",
+                    "value": 3,
+                  },
+                  {
+                    "display": "Inshirah",
+                    "value": 4,
+                  }
+                ],
+                textField: 'display',
+                valueField: 'value',
+                filterable: true,
+                required: true,
+                value: null,
+                onSaved: (value) {
+                  print('The value is $value');
+                }),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {
+                    _titleController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('CREATE'),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return MyHomePage();
+                    }));
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
