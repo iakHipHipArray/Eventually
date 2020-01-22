fdfee1174c66943ea770f3472fb6c72b3b59973e
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2020-01-22 11:00:17 +0000

    created list items for each notification

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -8,7 +8,7 @@ class NotificationsPage extends StatefulWidget {
 
 class _NotificationsPageState extends State<NotificationsPage> {
   List _notifications = [];
-  var keys;
+  List keys;
   var count = 0;
 
   getNotifications() {
@@ -17,36 +17,44 @@ class _NotificationsPageState extends State<NotificationsPage> {
         .document('rmpillar')
         .get()
         .then((notifications) {
-      keys = notifications.data.keys;
-      _notifications.add(notifications);
+      setState(() {
+        keys = notifications.data.keys.toList();
+        for (var i = 0; i < keys.length; i++) {
+          _notifications.add(notifications.data[keys[i]]);
+        }
+        count++;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (count < 1) {
-      getNotifications();
-    }
+    if (count < 1) getNotifications();
 
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: ListView.builder(
-            itemExtent: 80.0,
-            itemCount: _notifications.length,
-            itemBuilder: (context, index) =>
-                _buildListItem(context, _notifications[index]),
-          ),
-        )
-      ],
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text('Notifications'),
+          Expanded(
+            child: ListView.builder(
+              itemExtent: 80.0,
+              itemCount: _notifications.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, _notifications[index]),
+            ),
+          )
+        ],
+      ),
     );
   }
 
   Widget _buildListItem(BuildContext context, data) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[Text(data['body'])],
+    return Card(
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[Text(data['body'])],
+        ),
       ),
     );
   }
