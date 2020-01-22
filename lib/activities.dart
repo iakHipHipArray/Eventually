e1dcf6cc8012676d9c63f8396aa3f8d77493f652
Robin Pillar <rpillar72@gmail.com>
2020-01-22 10:54:17 +0000

    created Activites list and ability to add new activities

--- lib/activities.dart
+++ lib/activities.dart
@@ -2,38 +2,60 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Activity extends StatelessWidget {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+
+  newActivity(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Suggest Activity'),
+            content: Column(
+              children: <Widget>[
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(hintText: "Activity"),
+                ),
+                TextField(
+                  controller: _descriptionController,
+                  decoration: InputDecoration(hintText: "Description"),
+            ),
+            FlatButton(
+                child: new Text('SUGGEST'),
+                onPressed: () {
+                  final now = (new DateTime.now()).millisecondsSinceEpoch.toString();
+                  final activity = {now:{'name':_nameController.text,'body':_descriptionController.text,'votes':0}};
+                  print(activity);
+                  Firestore.instance.collection('activities').document('event1').updateData(activity);
+                   Navigator.of(context).pop();
+                },)
+              ],
+            ),
+            
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
+    return Column(
       children: <Widget>[
-        Text(
-          'Activity overview',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 38),
-        ),
-        Card(
-          child: Column(children: <Widget>[
-            Text('My event'),
-            Text(
-                '술마시자 애두랑 히히히ㅣㅎ히 신나라 큐큐큨큐큐'),
-          ]),
-        ),
-        Image(
-          image: NetworkImage(
-              'https://ichef.bbci.co.uk/news/660/cpsprodpb/9E04/production/_100525404_gettyimages-475705672-2.jpg'),
-        ),
-        Card(
-          child: Row(
-            children: <Widget>[Icon(Icons.calendar_today), Text('Date: TBC')],
-          ),
-        ),
-        Expanded(
-          child: Container(
-            child: _buildBody(context),
-          ),
-        )
+    Expanded(
+      child: _buildBody(context),
+    ),
+    RaisedButton(
+      child: Icon(
+        Icons.add,
+        semanticLabel: 'SUggest a new activity',
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      onPressed: () {
+        newActivity(context);
+      }
+    )
       ],
-    ));
+    );
   }
 }
 
@@ -46,22 +68,24 @@ Widget _buildBody(BuildContext context) {
     builder: (context, snapshot) {
       if (!snapshot.hasData) return Text('LOADING');
       var keys = snapshot.data.data.keys.toList();
-      var activity= snapshot.data.data[keys[0]]['name'].toString();
-      var description = snapshot.data.data[keys[0]]['body'].toString();
-      print (activity);
-      print (description);
-     
       return ListView.builder(
           itemExtent: 80.0,
           itemCount: keys.length,
-          itemBuilder: (context, index) => Card(
-                child: Row(
+          itemBuilder: (context, index) {
+            final activity= snapshot.data.data[keys[index]]['name'].toString();
+            final description = snapshot.data.data[keys[index]]['body'].toString();
+            return Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(activity),
                     Text(description)
                   ],
                 ),
-              ));
+            );
+          },
+      );
     },
   );
 }
+
