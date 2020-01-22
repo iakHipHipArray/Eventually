709974f090dd466a0b3245dd4ef08ed57d7f9e59
Robin Pillar <rpillar72@gmail.com>
2020-01-22 11:37:34 +0000

    implemented voter widget

--- lib/activities.dart
+++ lib/activities.dart
@@ -1,3 +1,4 @@
+import 'package:EVENTually/voter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -20,16 +21,16 @@ class Activity extends StatelessWidget {
                 TextField(
                   controller: _descriptionController,
                   decoration: InputDecoration(hintText: "Description"),
-            ),
-            FlatButton(
-                child: new Text('SUGGEST'),
-                onPressed: () {
-                  final now = (new DateTime.now()).millisecondsSinceEpoch.toString();
-                  final activity = {now:{'name':_nameController.text,'body':_descriptionController.text,'votes':0}};
-                  print(activity);
-                  Firestore.instance.collection('activities').document('event1').updateData(activity);
-                   Navigator.of(context).pop();
-                },)
+                ),
+                FlatButton(
+                  child: new Text('SUGGEST'),
+                  onPressed: () {
+                    final now = (new DateTime.now()).millisecondsSinceEpoch.toString();
+                    final activity = {now:{'name':_nameController.text,'body':_descriptionController.text,'votes':0}};
+                    Firestore.instance.collection('activities').document('event1').updateData(activity);
+                    Navigator.of(context).pop();
+                  },
+                )
               ],
             ),
             
@@ -39,22 +40,24 @@ class Activity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-    Expanded(
-      child: _buildBody(context),
-    ),
-    RaisedButton(
-      child: Icon(
-        Icons.add,
-        semanticLabel: 'SUggest a new activity',
+    return Scaffold(
+          body: Column(
+        children: <Widget>[
+      Expanded(
+        child: _buildBody(context),
       ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      onPressed: () {
-        newActivity(context);
-      }
-    )
-      ],
+      RaisedButton(
+        child: Icon(
+          Icons.add,
+          semanticLabel: 'SUggest a new activity',
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: () {
+          newActivity(context);
+        }
+      )
+        ],
+      ),
     );
   }
 }
@@ -72,16 +75,22 @@ Widget _buildBody(BuildContext context) {
           itemExtent: 80.0,
           itemCount: keys.length,
           itemBuilder: (context, index) {
-            final activity= snapshot.data.data[keys[index]]['name'].toString();
+            final activity = snapshot.data.data[keys[index]]['name'].toString();
             final description = snapshot.data.data[keys[index]]['body'].toString();
             return Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Row(
                   children: <Widget>[
-                    Text(activity),
-                    Text(description)
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(activity),
+                        Text(description),
+                      ],
+                    ),
+                    Voter('activities','event1',keys[index])
                   ],
                 ),
+                
             );
           },
       );
---
+++ lib/voter.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Voter extends StatefulWidget {
+  final collection;
+  final document;
+  final item;
+
+  Voter(this.collection, this.document, this.item);
+  
+  @override
+  _VoterState createState() => new _VoterState();
+}
+
+class _VoterState extends State<Voter> {
+  bool _isButtonDisabled;
+  @override
+  void initState() {
+    _isButtonDisabled = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.document);
+    return _buildBody(context);
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance
+          .collection(widget.collection)
+          .document(widget.document)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Text('0');
+        return _buildVoter(context, snapshot.data.data);
+      },
+    );
+  }
+
+  Widget _buildVoter(BuildContext context, snapshot) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+            child: Icon(
+              _isButtonDisabled ? (Icons.favorite) : (Icons.favorite_border),
+            ),
+            onTap: () {
+              _isButtonDisabled
+                  ? null
+                  : Firestore.instance
+                      .collection(widget.collection)
+                      .document(widget.document)
+                      .updateData({
+                      '${widget.item}.votes': snapshot['${widget.item}']['votes'] + 1
+                    });
+              setState(() {
+                _isButtonDisabled = true;
+              });
+            }),
+        Text(snapshot['${widget.item}']['votes'].toString())
+      ],
+    );
+  }
+}
