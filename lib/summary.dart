dbd5bab262ca4b8cee04c724c9afd75286de4532
Robin Pillar <rpillar72@gmail.com>
2020-01-23 10:46:21 +0000

    finished Summary page

--- lib/summary.dart
+++ lib/summary.dart
@@ -1,7 +1,71 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+class DropDown extends StatefulWidget {
+  final document;
+  final dates;
+  final locations;
+  DropDown(this.document,this.dates,this.locations);
+  @override
+  _DropDownState createState() => _DropDownState();
+}
+
+class _DropDownState extends State<DropDown> {
+  String dropDownDate = '02-02-2020';
+  String dropDownLocation = 'Sheffield';
+  @override
+  Widget build(BuildContext context) {
+              print(widget.dates);
+    return Column(
+      children: <Widget>[
+        new DropdownButton<String>(
+          value: dropDownDate,
+          items: widget.dates
+            .map<DropdownMenuItem<String>>((value) {
+              return new DropdownMenuItem<String>(
+                value: value,
+                child: new Text(value),
+              );
+            }
+          ).toList(),
+          onChanged: (String newValue) {
+            setState(() {
+              dropDownDate = newValue;
+            });
+          }
+        ),
+        new DropdownButton<String>(
+          value: dropDownLocation,
+          items: widget.locations
+            .map<DropdownMenuItem<String>>((value) {
+              return new DropdownMenuItem<String>(
+                value: value,
+                child: new Text(value),
+              );
+            }
+          ).toList(),
+          onChanged: (String newValue) {
+            setState(() {
+              dropDownLocation = newValue;
+            });
+          }
+        ),
+        FlatButton(
+          child: Text('FINALIZE'),
+          onPressed: () {
+            final data = {'date':dropDownDate,'location':dropDownLocation};
+            Firestore.instance.collection('events').document(widget.document).updateData(data);
+            Navigator.pop(context);
+          },
+
+        )
+      ],
+    );
+  }
+}
+
 class Summary extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -11,6 +75,8 @@ class Summary extends StatelessWidget {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return Text('LOADING');
         final event = snapshot.data.data;
+        final date = event['date'] == null ? 'Date: TBC' : event['date'];
+        final location = event['location'] == null ? 'Location: TBC' : event['location'];
         // final image = event['image'] ? event['image'] : '';
         // print(image);
         return Container(
@@ -38,19 +104,21 @@ class Summary extends StatelessWidget {
             ),
             Card(
               child: Row(
-                children: <Widget>[Icon(Icons.location_on), Text('Date: TBC')],
+                children: <Widget>[Icon(Icons.location_on), Text(location)],
               ),
             ),
             Card(
               child: Row(
-                children: <Widget>[Icon(Icons.calendar_today), Text('Date: TBC')],
+                children: <Widget>[Icon(Icons.calendar_today), Text(date)],
               ),
             ),
             Expanded(
               child: Container(
                 child: _buildBody(context),
               ),
-            )
+            ),
+            if(event['date'] == null) finalizeButton(context)
+            
           ],
         ));
       }
@@ -81,5 +149,27 @@ Widget _buildBody(BuildContext context) {
                 ),
               ));
     },
+  );
+}
+
+Widget finalizeButton(BuildContext context) {
+  finalizeEvent(BuildContext context) async {
+  final locations = ['Sheffield','Leeds', 'Slough','Edinburgh'];
+  final dates = ['02-02-2020','21-02-2020','01-03-2020'];
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Finalize Details'),
+        content: DropDown('event1',dates,locations),
+      );
+    });
+  }
+  return RaisedButton(
+    child: Text('Finalize'),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    onPressed: () {
+      finalizeEvent(context);
+    }
   );
 }
